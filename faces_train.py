ea1886aec873669c4043fff061f4fce562c63d9d
Leonardo <leo_coelho_@hotmail.com>
2019-11-12 15:54:45 -0300

    Details changed

--- faces_train.py
+++ faces_train.py
@@ -4,12 +4,16 @@ import cv2
 from PIL import Image
 import pickle
 
+
+#========= Folder path
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 image_dir = os.path.join(BASE_DIR, "images")
 
+#========= Haar Cascade and Recogniter
 face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 
+#========= Global variables
 current_id = 0
 label_ids = {}
 x_train = []
@@ -17,17 +21,22 @@ y_labels = []
 
 for root, dirs, files in os.walk(image_dir):
     for file in files:
+
+#========= Search for any .png or .jpg files in /images/
         if file.endswith("png") or file.endswith("jpg"):
             path = os.path.join(root, file)
             label = os.path.basename(root).replace(" ", "-").lower()
             #print(label, path)
+
+#========= Update labels vector
             if not label in label_ids:
                 label_ids[label] = current_id
                 current_id += 1
-                
-            id_ = label_ids[label]
+
+            id_ = label_ids[label] #label is based on folder name
             #print(label_ids)
 
+#========= Adjust images (grayscale)
             pil_image = Image.open(path).convert("L")   #grayscale
             size = (550, 550)
             final_image = pil_image.resize(size, Image.ANTIALIAS)
@@ -36,6 +45,7 @@ for root, dirs, files in os.walk(image_dir):
 
             faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.5, minNeighbors=5)
 
+#========= Identify ROI (region of interest) and make element tag and label
             for (x, y, w, h) in faces:
                 rdi = image_array[y:y+h, x:x+w]      #[Y-start:Y-end, X-start:X-end]
                 x_train.append(rdi)
@@ -46,9 +56,12 @@ for root, dirs, files in os.walk(image_dir):
 #print(x_train)
 #print(label_ids)
 
+#========= Generate bin archieve with labels data
 with open("labels.pickle", 'wb') as f:
     pickle.dump(label_ids, f)
 
+#========= Generate YML with all info data based on image pixel and label Id
 recognizer.train(x_train, np.array(y_labels))
 recognizer.save("trainner.yml")
+
 print("Trainning Complete!")
--- faces_train_specific.py
+++ faces_train_specific.py
@@ -3,9 +3,10 @@ import numpy as np
 from PIL import Image
 
 
-elem = []
+elem = []   # Element name array
 
 
+# Convert char array in a string array
 def makeArray(list_char):
     word = ''
     for c in list_char:
@@ -29,13 +30,18 @@ except:
 elem = makeArray(elemName)
 
 
+#========= Add every element on the trainning
 for i in range(len(elem)):
+
+#========= Folder path
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     image_dir = os.path.join(BASE_DIR, "images/" + elem[i])
 
+#========= Haar Cascade and Recogniter
     face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
     recognizer = cv2.face.LBPHFaceRecognizer_create()
 
+#========= Global variables
     current_id = 0
     label_ids = {}
     x_train = []
@@ -43,10 +49,14 @@ for i in range(len(elem)):
 
     for root, dirs, files in os.walk(image_dir):
         for file in files:
+
+#========= Search for any .png or .jpg files in /images/
             if file.endswith("png") or file.endswith("jpg"):
                 path = os.path.join(root, file)
                 label = os.path.basename(root).replace(" ", "-").lower()
                 #print(label, path)
+
+#========= Update labels vector
                 if not label in label_ids:
                     label_ids[label] = current_id
                     current_id += 1
@@ -54,6 +64,7 @@ for i in range(len(elem)):
                 id_ = label_ids[label]
                 #print(label_ids)
 
+#========= Adjust images (grayscale)
                 pil_image = Image.open(path).convert("L")   #grayscale
                 size = (550, 550)
                 final_image = pil_image.resize(size, Image.ANTIALIAS)
@@ -62,6 +73,7 @@ for i in range(len(elem)):
 
                 faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.5, minNeighbors=5)
 
+#========= Identify ROI (region of interest) and make element tag and label
                 for (x, y, w, h) in faces:
                     rdi = image_array[y:y+h, x:x+w]      #[Y-start:Y-end, X-start:X-end]
                     x_train.append(rdi)
@@ -72,9 +84,12 @@ for i in range(len(elem)):
 #print(x_train)
 #print(label_ids)
 
+#========= Generate bin archieve with labels data
 with open("labels.pickle", 'wb') as f:
     pickle.dump(label_ids, f)
 
+#========= Generate YML with all info data based on image pixel and label Id
 recognizer.train(x_train, np.array(y_labels))
 recognizer.save("trainner.yml")
+
 print("Trainning Complete!")
